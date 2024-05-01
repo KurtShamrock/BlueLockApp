e90b11180c175d0dc588eaa39d9875c99fb31a39
Mike1702 <maipham1712002@gmail.com>
2024-05-01 21:24:15 +0700

    Second version: Kiểm tra xem có mật khẩu chưa, nếu chưa có thì yêu cầu nhập mật khẩu mới, xác nhận thêm 1 lần. Nếu đã có mật khẩu thì yêu cầu nhập mật khẩu rồi chuyển sang giao diện đổi mật khẩu mới. Các tọa độ mật khẩu được lưu lại và mã hóa AES, sau đó lưu vào sharePreferences. Khi kiểm tra mật khẩu, app lấy đoạn được mã hóa, giải mã hóa AES dựa trên khóa có sẵn để ra tọa độ ban đầu, kiểm tra xem các tọadđộ vừa nhập có nằm trong phạm vi của các tọa độ mật khẩu được lưu sẵn không. Nếu thỏa mãn thì trả về kết quả mật khẩu đúng, nếu sai thì yêu cầu nhập lại

--- app/src/main/java/com/example/appintern/NewPassword.java
+++ app/src/main/java/com/example/appintern/NewPassword.java
@@ -1,6 +1,7 @@
 package com.example.appintern;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,8 +11,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -42,13 +45,35 @@ public class NewPassword extends AppCompatActivity {
                 coordinateList.add(point);
                 drawCircleAroundPoint(point);
                 if (coordinateList.size() == MAX_COORDINATES) {
-                    goToRecheckCoordinates();
+                    Button nextBtn = findViewById(R.id.nextBtn);
+                    Button cancelBtn = findViewById(R.id.cancelBtn);
+                    nextBtn.setVisibility(View.VISIBLE);
+                    cancelBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            goToRecheckCoordinates();
+                        }
+                    });
+                    cancelBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            resetActivity();
+                        }
+                    });
+                }
+                if (coordinateList.size() > MAX_COORDINATES) {
+                    resetActivity();
                 }
                 @SuppressLint("DefaultLocale") String message = String.format("Coordinates: (%d, %d)", x, y);
                 Log.d(NewPassword.DEBUGTAG, message);
                 return false;
             }
         });
+    }
+
+    private void resetActivity() {
+        recreate();
     }
     private void goToRecheckCoordinates() {
         Intent intent = new Intent(this, RecheckPassword.class);
